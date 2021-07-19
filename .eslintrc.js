c6b8b548400dd98ebf14d4e6457ce7a854e8b7c9
Karlis Zalite <karlis.zalite@gmail.com>
2021-07-19 14:17:07 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+      jest: true,
+      node: true
+    },
+    parser: '@typescript-eslint/parser',
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+        modules: true
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module'
+    },
+    plugins: ['react', '@typescript-eslint', 'react-hooks'],
+    rules: {
+      'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx', 'tsx']}],
+      'react/forbid-prop-types': [0, {forbid: ['any']}],
+      'react/prop-types': 0,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  };
+  
