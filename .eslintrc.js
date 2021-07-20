854ba9c70606fb7d6bcc665cc7de1c2c9f9af1cb
Karlis Zalite <karlis.zalite@gmail.com>
2021-07-20 14:27:02 +0200

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,29 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      jest: true,
-      node: true
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
     },
-    parser: '@typescript-eslint/parser',
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly'
-    },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-        modules: true
-      },
-      ecmaVersion: 2018,
-      sourceType: 'module'
-    },
-    plugins: ['react', '@typescript-eslint', 'react-hooks'],
-    rules: {
-      'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx', 'tsx']}],
-      'react/forbid-prop-types': [0, {forbid: ['any']}],
-      'react/prop-types': 0,
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn'
-    }
-  };
-  
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx', 'tsx']}],
+    'react/forbid-prop-types': [0, {forbid: ['any']}],
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
+import {Box, Grid, Paper} from '@material-ui/core';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LookupMap from './components/LookupMap/LookupMap';
+import ResultsMap from './components/ResultsMap/ResultsMap';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{marginTop: '20px'}}>
+      <Grid container item xs={12} justifyContent="center">
+        <Grid container item xs={8} spacing={2} component={Paper}>
+          <Grid item xs={6}>
+            <LookupMap />
+          </Grid>
+          <Grid item xs={6}>
+            <ResultsMap />
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
